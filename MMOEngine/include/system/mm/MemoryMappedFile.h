f54496312995c6be408cdd8bb4257c68e6a4ad66
TheAnswer <TheAnswer@c3d1530f-68f5-4bd0-87dc-8ef779617e40>
2011-12-25 05:21:15 +0000

    [updated] engine lion64 lib
    git-svn-id: http://svn2.assembla.com/svn/swgemu/engine3public@3493 c3d1530f-68f5-4bd0-87dc-8ef779617e40

---
+++ MMOEngine/include/system/mm/MemoryMappedFile.h
@@ -0,0 +1,34 @@
+/*
+Copyright (C) 2007 <SWGEmu>. All rights reserved.
+Distribution of this file for usage outside of Core3 is prohibited.
+*/
+
+#ifndef MEMORYMAPPEDFILE_H_
+#define MEMORYMAPPEDFILE_H_
+
+#include "Heap.h"
+
+namespace sys {
+  namespace mm {
+
+	class MemoryMappedFile : public Heap {
+		const char* fileName;
+		uint32 fileSize;
+
+	public:
+		MemoryMappedFile(const char* name, uint32 size);
+
+		void create() {
+			Heap::create(fileSize);
+		}
+
+	protected:
+		void openDevice();
+	};
+
+} // namespace mm
+} // namespace sys
+
+using namespace sys::mm;
+
+#endif /* MEMORYMAPPEDFILE_H_ */
