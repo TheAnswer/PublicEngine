3af901c58c2a9763c11714ebc88b05880e028ab0
Victor Popovici <victorpopovici@gmail.com>
2019-09-01 08:17:09 +0000

    [fixed] cleaned up code

--- MMOEngine/include/engine/log/Logger.h
+++ MMOEngine/include/engine/log/Logger.h
@@ -7,10 +7,6 @@
 #define LOGGER_H_
 
 #ifdef NDEBUG
-#define DISABLE_DEBUG_LOG
-#endif
-
-#ifdef NDEBUG
 #define E3_INFO(x)
 #define E3_DEBUG(x)
 #define E3_WARNING(x)
@@ -185,20 +181,9 @@ namespace engine {
 			}
 		}
 
-#ifdef DISABLE_DEBUG_LOG
-		void debug(const char* msg) const {
-		}
-
-		void debug(const String& msg) const {
-		}
-
-		void debug(const StringBuffer& msg) const {
-		}
-#else
 		void debug(const char* msg) const;
 		void debug(const String& msg) const;
 		void debug(const StringBuffer& msg) const;
-#endif
 
 		LoggerHelper debug() const {
 			return LoggerHelper(*this, LogLevel::DEBUG, false);
@@ -213,7 +198,7 @@ namespace engine {
 		}
 
 		bool hasToLog(LogLevel level) const {
-			return logLevel >= level;
+			return logLevel >= level || (doGlobalLog && globalLogFile && globalLogLevel >= level);
 		}
 
 		static void getTime(String& time, bool getFull = true);
